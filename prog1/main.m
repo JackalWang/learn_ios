ed847214435818cf5f46b77c62526ce93c321422
jackal <jackal@yahoo-inc.com>
2015-06-12 10:21:46 +0800

    upload code

---
+++ prog1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  prog1
+//
+//  Created by Jackal Wang on 2015/6/10.
+//  Copyright (c) 2015年 Jackal Wang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        NSLog(@"Programming in Objective-C is even more fun!");
+    }
+    return 0;
+}
